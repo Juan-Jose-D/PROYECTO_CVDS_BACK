1de4d1908a5da14bcf958bb82e2db027f57a777a
Juan José Díaz <81777362+Juan-Jose-D@users.noreply.github.com>
2025-02-23 18:32:36 -0500

    Proyecto spring

---
+++ backend/src/test/java/edu/eci/cvds/ReservationProject/ReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ReservationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
